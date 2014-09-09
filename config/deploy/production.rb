25e9112d444d70ab3e469dd51300b8beaa4f3522
yesteph <stephane.teyssier@gmail.com>
2014-09-09 18:45:24 +0200

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{ec2-user@ip-172-30-0-178.ec2.internal}
+#role :web, %w{deploy@example.com}
+#role :db,  %w{deploy@example.com}
 
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'ip-172-30-0-178.ec2.internal', user: 'ec2-user', roles: %w{app}, my_property: :my_value
 
 
 # Custom SSH Options
@@ -25,11 +25,11 @@ server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+  set :ssh_options, {
+#   keys: %w(/home/rlisowski/.ssh/id_rsa),
+    forward_agent: true,
+ #  auth_methods: %w(password)
+  }
 #
 # And/or per server (overrides global)
 # ------------------------------------
